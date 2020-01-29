676fa319ef8bea0b8f84cbbf1185bfabda855df1
Park <tiffks82@naver.com>
2020-01-29 17:39:46 +0900

    Park Add Btn 2020-01-29

--- CnsiCalculator/Form1.Designer.cs
+++ CnsiCalculator/Form1.Designer.cs
@@ -50,6 +50,7 @@
             this.btn_resultMemorySave = new System.Windows.Forms.Button();
             this.btn_equalSignInput = new System.Windows.Forms.Button();
             this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // txt_numberInputView
@@ -271,11 +272,21 @@
             this.listBox1.Size = new System.Drawing.Size(163, 280);
             this.listBox1.TabIndex = 21;
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(233, 259);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 22;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(395, 301);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.btn_equalSignInput);
             this.Controls.Add(this.btn_resultMemorySave);
@@ -331,6 +342,7 @@
         private System.Windows.Forms.Button btn_resultMemorySave;
         private System.Windows.Forms.Button btn_equalSignInput;
         private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
